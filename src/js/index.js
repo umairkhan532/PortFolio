5d1c3d28e512a2b2693f8ad64b0d7835cb804a61
Mohammed Umair Kaleem Khan <74628138+umairkhan532@users.noreply.github.com>
2022-10-06 15:15:24 -0500

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,5 @@ import resume from "../assets/resume.pdf";
 export default function initApp() {
   initSr();
   initTilt();
-  // comment this if you don't want to attach your resume
   addResume(resume);
 }
